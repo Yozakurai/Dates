48b4a4c835d4ee22877ca7ddf6945fecdc8bc9fd
Yozakurai <deivids.saulitis@gmail.com>
2023-03-13 00:44:15 +0200

    rekursijasv2

--- Rekursija/Rekursija/Program.cs
+++ Rekursija/Rekursija/Program.cs
@@ -19,20 +19,24 @@
         Console.Write("Progresijas sākums: ");
         int prosakums = int.Parse(Console.ReadLine());
         progresija(element,prosolis,prosakums);
-
+//
+//uzd1
+//
 static int Rekursijasfun(int skievade)
 {
-    if (skievade == 0) //Ja skaitlis ir 0, tad izvada 0
+    if (skievade == 0) 
     {
         return 0;
     }
-    else //Ja nav = 0, tad aprēķina skaitli
+    else 
     {
         return skievade % 10 + Rekursijasfun(skievade / 10); //ievadīto pēdējo skaitli saskaitam ar funkciju, kas izsauc sevi ar skaitli, kas iegūts, dalot 'skievade' ar 10.
                                                              //Šī funkcija tiek izsaukta ar jaunu skaitli(skievade / 10) tik ilgi, kamēr skaitlis ir lielāks par 0.
     }
 }
-
+//
+//uzd2
+//
 static int progresija(int element, int prosolis, int prosakums)
 {
     Console.WriteLine(prosakums + prosolis);
