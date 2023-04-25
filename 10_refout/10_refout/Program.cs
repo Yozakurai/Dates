a48d4fe997b6f492d0441e78b763f5a12c6db476
Yozakurai <deivids.saulitis@gmail.com>
2023-04-25 10:54:36 +0300

    10_refout

---
+++ 10_refout/10_refout/Program.cs
@@ -0,0 +1,104 @@
+﻿
+
+/*
+Calculator objekts = new Calculator();
+Console.WriteLine("Ievadiet x un y: ");
+int x, y;
+string X = Console.ReadLine();
+string Y = Console.ReadLine();
+int.TryParse(X, out x);
+int.TryParse(Y, out y);
+Console.WriteLine($"x vertiba: {x}");
+Console.WriteLine($"x vertiba: {y}");
+objekts.Kapinat(ref x);
+Console.WriteLine($"Kapinat: {x} ");
+int.TryParse(X, out x);
+objekts.Reizinat(ref x, ref y);
+Console.WriteLine($"Reizinat: {x}");
+*/
+Console.WriteLine("\n2.uzd\n");
+// 2.uzdevums
+Console.WriteLine("Cik daudz skaitļu būs masīva:");
+int skaits = int.Parse(Console.ReadLine());
+Console.WriteLine("Ievadiet masiva vertibas: ");
+int[] values = new int[skaits];
+for (int i = 0; i < skaits; i++)
+{
+    values[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine($"Min: {MansMath.Min(values)}");
+Console.WriteLine($"Max: {MansMath.Max(values)}");
+Console.WriteLine($"Min: {MansMath.Min(2, 3, 5, 1, 6, 8, 3)}");
+Console.WriteLine($"Max: {MansMath.Max(2, 3, 5, 1, 6, 8, 3)}");
+
+Console.WriteLine("\n3.uzd\n");
+
+Console.WriteLine("Ievadiet masīva garumu:");
+skaits = int.Parse(Console.ReadLine());
+int[] mas = new int[skaits];
+Random rand = new Random();
+for (int i = 0; i < skaits; i++)
+{
+    mas[i] = rand.Next(1, 11);
+}
+int Skaits;
+Console.WriteLine($"Para skaitla summa: {ParaSkaitluSumma(mas, out Skaits)}"); //izvada para skaitlu summu
+Console.WriteLine($"Para skaitla skaits {Skaits}"); //izvada para skaitlu skaitu
+
+static int ParaSkaitluSumma(int[] mas, out int paraSkaitluSkaits)
+{
+    paraSkaitluSkaits = 0;
+    int summa = 0;
+    foreach (int n in mas)
+    {
+        if (n % 2 == 0)
+        {
+            Console.Write($"{n} ");
+            summa += n;
+            paraSkaitluSkaits++;
+        }
+    }
+    return summa;
+    //return paraSkaitluSkaits;
+}
+//1.uzd
+public class Calculator
+{
+    public void Kapinat(ref int x)
+    {
+        x = (int)Math.Pow(x, -1 / x);
+    }
+    public void Reizinat(ref int x, ref int y)
+    {
+        x = x * y;
+    }
+}
+//2.uzd
+
+public class MansMath
+{
+    public static int Min(params int[] values)
+    {
+        int min = values[0];
+        for (int i = 0; i < values.Length; i++)
+        {
+            if (values[i] < min)
+            {
+                min = values[i];
+            }
+        }
+        return min;
+    }
+    public static int Max(params int[] values)
+    {
+        int max = values[0];
+        for (int i = 0; i < values.Length; i++)
+        {
+            if (values[i] > max)
+            {
+                max = values[i];
+            }
+        }
+        return max;
+    }
+}
---
+++ 9_structure/9_structure/Program.cs
@@ -0,0 +1,90 @@
+﻿Console.Write("Ierakstiet darbinieku skaitu: ");
+int darbinieku_skaits = int.Parse(Console.ReadLine());
+Darbinieks[] darbinieks;
+darbinieks = new Darbinieks[darbinieku_skaits];
+for (int i = 0; i < darbinieku_skaits; i++) //reģistrē darbiniekus
+{
+    darbinieks[i] = new Darbinieks();
+    darbinieks[i].Registret();
+}
+
+///izvēlne
+do
+{
+    Console.WriteLine();
+    Console.WriteLine("\nLūdzu izvēlaties metodi... \n1 - Ievadit darbinieku\n\n2 - Izvadit uz ekrana\n\n3 - Izvadit konkretu darbinieku\n");
+    int izvele;
+    Console.Write("Jūsu izvēle: ");
+    izvele = int.Parse(Console.ReadLine());
+
+    if (izvele == 1)
+    {
+
+        Array.Resize(ref darbinieks, darbinieks.Length + 1); //palielina masīvu
+        for (int i = darbinieku_skaits; i < darbinieku_skaits + 1; i++) //ievada datus
+        {
+            darbinieks[i] = new Darbinieks();
+            darbinieks[i].Registret();
+        }
+        darbinieku_skaits++; 
+    }
+    else if (izvele == 2)
+    {
+        Console.WriteLine();
+        Console.WriteLine("ID\tVards\tUzvards\t\tBernu skaits\tAmats\t\tSlodze\n");
+        foreach (Darbinieks n in darbinieks) //izvada datus
+        {
+            n.Izvadit();
+        }
+    }
+    else if (izvele == 3)
+    {
+        Console.Write("\nIevadiet darbinieka ID: ");
+        int id_meklet = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        IzvaditPecId(id_meklet, darbinieks);
+    }
+} while (true);
+
+void IzvaditPecId(int id_meklet, Darbinieks[] darbinieks)
+{
+    Console.WriteLine("ID\tVards\tUzvards\t\tBernu skaits\tAmats\t\tSlodze\n");
+    foreach (Darbinieks n in darbinieks)
+    {
+        if (n.id == id_meklet)
+        {
+            n.Izvadit();
+        }
+    }
+}
+public struct Darbinieks
+{
+    public int id;
+    public string vards;
+    public string uzvards;
+    public int bernu_sk;
+    public string amats;
+    public bool slodze;
+    public void Registret()
+    {
+        Console.Write("\nId: ");
+        id = int.Parse(Console.ReadLine());
+        Console.Write("\nVārds: ");
+        vards = Console.ReadLine();
+        Console.Write("\nUzārds: ");
+        uzvards = Console.ReadLine();
+        Console.Write("\nBērnu skaits: ");
+        bernu_sk = int.Parse(Console.ReadLine());
+        Console.Write("\nAmats: ");
+        amats = Console.ReadLine();
+        Console.Write("\nSlodze: ");
+        slodze = bool.Parse(Console.ReadLine());
+        Console.Write("\n......");
+        Console.Write("\n\n");
+    }
+    public void Izvadit()
+    {
+
+        Console.WriteLine($"{id}\t{vards}\t{uzvards}\t\t{bernu_sk}\t\t{amats}\t\t{slodze}\n");
+    }
+}
