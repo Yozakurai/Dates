5fe410e29e9d756d50baaa1f28f21bfd06759c1b
Yozakurai <deivids.saulitis@gmail.com>
2023-05-16 10:07:44 +0300

    mantosana

---
+++ 14_mantosana/14_mantosana/Program.cs
@@ -0,0 +1,162 @@
+﻿Veikals obj1 = new Veikals();
+obj1.Registret();
+obj1.Izvadit();
+class Prece
+{
+    public string Nosaukums;
+    public double Iepirksanas_cena;
+    public void Izvaditt()
+    {
+        Console.WriteLine($"\nNosaukums: {Nosaukums}");
+        Console.WriteLine($"Iepirksanas cena: {Iepirksanas_cena}");
+    }
+}
+class Partikas_Prece : Prece
+{
+    public DateTime Deriguma_termins;
+    public bool Ir_alregisks;
+    public string Mervieniba;
+    public double Pardosanas_cena;
+    public void Registret()
+    {
+        Console.WriteLine("\nIevadiet partikas preces nosaukumu:");
+        Nosaukums = Console.ReadLine();
+        Console.WriteLine($"Ievadiet preces {Nosaukums} iepirksanas cenu:");
+        Iepirksanas_cena = Double.Parse(Console.ReadLine());
+        Console.Write("Ievadiet deriguma terminu dd/MM/yyyy: ");
+        Deriguma_termins = DateTime.Parse(Console.ReadLine());
+        Console.Write("Ir alergisks? True/False: ");
+        Ir_alregisks = bool.Parse(Console.ReadLine());
+        Console.Write("Mervieniba: ");
+        Mervieniba = Console.ReadLine();
+        Pardosanas_cena = Iepirksanas_cena + Iepirksanas_cena * 0.3;
+    }
+    public void Izvadit()
+    {
+        Izvaditt();
+        Console.WriteLine($"Deriguma termins: {Deriguma_termins}");
+        Console.WriteLine($"Ir alergisks: {Ir_alregisks}");
+        Console.WriteLine($"Mervieniba: {Mervieniba}");
+        Console.WriteLine($"Pardosanas cena: {Pardosanas_cena}");
+    }
+}
+class Saimniecibas_Prece : Prece
+{
+    public string Materials;
+    public bool Ir_bisatama;
+    public double Pardosanas_cena;
+    public void Registret()
+    {
+        Console.WriteLine("\nIevadiet saimniecības preces nosaukumu:");
+        Nosaukums = Console.ReadLine();
+        Console.WriteLine($"Ievadiet preces {Nosaukums} iepirksanas cenu:");
+        Iepirksanas_cena = Double.Parse(Console.ReadLine());
+        Console.Write("Materials: ");
+        Materials = Console.ReadLine();
+        Console.Write("Ir bistams? True/False: ");
+        Ir_bisatama = bool.Parse(Console.ReadLine());
+        Pardosanas_cena = Iepirksanas_cena + Iepirksanas_cena * 0.5;
+    }
+    public void Izvadit()
+    {
+        Izvaditt();
+        Console.WriteLine($"Materials: {Materials}");
+        Console.WriteLine($"Ir bistams: {Ir_bisatama}");
+        Console.WriteLine($"Pardosanas cena: {Pardosanas_cena}");
+    }
+}
+class Veikals
+{
+    public string Nosaukums;
+    public int Partikas_precu_ckaits;
+    public int Saimniecibas_precu_skaits;
+    Partikas_Prece[] Partikas_preces;
+    Saimniecibas_Prece[] Saimniecibas_preces;
+    public void Registret()
+    {
+        Console.WriteLine("Ievadiet veikala nosaukumu:");
+        Nosaukums = Console.ReadLine();
+        Console.WriteLine("Ievadiet partikas precu skaitu:");
+        Partikas_precu_ckaits = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ievadiet saimniecibas precu skaitu:");
+        Saimniecibas_precu_skaits = int.Parse(Console.ReadLine());
+        Partikas_preces = new Partikas_Prece[Partikas_precu_ckaits];
+        for (int i = 0; i < Partikas_preces.Length; i++)
+        {
+            Partikas_preces[i] = new Partikas_Prece();
+            Partikas_preces[i].Registret();
+        }
+        Saimniecibas_preces = new Saimniecibas_Prece[Saimniecibas_precu_skaits];
+        for (int j = 0; j < Saimniecibas_preces.Length; j++)
+        {
+            Saimniecibas_preces[j] = new Saimniecibas_Prece();
+            Saimniecibas_preces[j].Registret();
+        }
+    }
+    public void Izvadit()
+    {
+        Console.WriteLine("\n=========================================================================");
+        Console.WriteLine($"\nVeikala nosaukums: {Nosaukums}");
+        Console.WriteLine($"Partikas precu skaits: {Partikas_precu_ckaits}");
+        Console.WriteLine($"Saimniecibas precu skaits: {Saimniecibas_precu_skaits}");
+        Console.WriteLine($"\nPartikas preces:");
+        for (int i = 0; i < Partikas_preces.Length; i++)
+        {
+            Partikas_preces[i].Izvadit();
+        }
+        Console.WriteLine("\nSaimniecibas preces:");
+        for (int j = 0; j < Saimniecibas_preces.Length; j++)
+        {
+            Saimniecibas_preces[j].Izvadit();
+        }
+        Veikala_tips();
+        Atlasit_arpus_termina();
+        Atlasit_bistamus();
+    }
+    public void Veikala_tips()
+    {
+        if (Partikas_precu_ckaits == 0)
+        {
+            Console.WriteLine("Veikala tips: Saimniecibas veikals");
+        }
+        else if (Saimniecibas_precu_skaits == 0)
+        {
+            Console.WriteLine("Veikala tips: Partikas viekals");
+        }
+        else
+        {
+            Console.WriteLine("Veikala tips: Lielveikals");
+        }
+    }
+    public void Atlasit_arpus_termina()
+    {
+        Console.WriteLine("\nDeriguma termins beidzas:");
+        DateTime currentDate = DateTime.Today;
+        for (int i = 0; i < Partikas_preces.Length; i++)
+        {
+            if (Partikas_preces[i].Deriguma_termins <= currentDate)
+            {
+                Console.WriteLine(Partikas_preces[i].Nosaukums);
+            }
+            else
+            {
+
+            }
+        }
+    }
+    public void Atlasit_bistamus()
+    {
+        Console.WriteLine("\nBistamas preces:");
+        for (int i = 0; i < Saimniecibas_preces.Length; i++)
+        {
+            if (Saimniecibas_preces[i].Ir_bisatama == true)
+            {
+                Console.WriteLine(Saimniecibas_preces[i].Nosaukums);
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ 15_rakstitaji_un_lasitaji/15_rakstitaji_un_lasitaji/Program.cs
@@ -0,0 +1,128 @@
+﻿//Izveidot klasi StudijuKurss ar public īpašībām:
+//  • Nosaukums string tipa;
+//  • Kredītpunkti int tipa;
+//  • EKredītpunkti double(izmantojot get – Kredītpunkti * 1, 5)
+//  • IrObligāts bool tipa;
+//Izveidot klasei StudijuKurss funkcijas:
+//  • ReadData() – nolasa no klaviatūras klases īpašību vērtības (izņemot
+//EKredītpunkti);
+//  • PrintData() – izvada klases vērtības uz ekrāna;
+//
+//Program klases funkcijas
+//  • FillArray(StudijuKurss[] kurss)-aizpilda masīvu ar vērtībām (izmantojot klases StudijuKurss funkciju ReadData());
+//  • PrintArray(StudijuKurss[] kurss) – saņem kā parametru StudijuKurss tipa masīvu un izvada visus masīva vērtības uz ekrāna (izmantojot klases StudijuKurss funkciju PrintData());
+//  • PrintArrayToFile(StudijuKurss[] kurss) – saņem kā parametru StudijuKurss tipa masīvu un saglabā masīva vērtības .txt failā, kur katru īpašību atdala ar „ ; ” vai citu simbolu;
+//  • StudijuKurss[] ReadArrayFromFile() – nolasa saglabāto.txt failu un saglāba vērtības StudijuKurss tipa masīvā (StudijuKurss[]). Pirms aizpildīt masīvu ar
+//    vērtībām ir nepieciešams noskaidrot masīva garumu, jo pirms nolasīšanas elementu skaits nav zināms! Funkcija atgriež StudijuKurss tipa masīvu!
+//Funkcijā Main():
+//  • Izveidot pirmo StudijuKurss tipa masīvu (StudijuKurss[]);
+//  • Aizpildīt masīvu ar vērtībām (funkcija FillArray()) un ierakstīt.txt failā (funkcija PrintArrayToFile());
+//  • Izveidot otro StudijuKurss tipa masīvu (StudijuKurss[]);
+//  • Nolasīt no faila informāciju uz otro masīvu (funkcija ReadArrayFromFile()) un izvadīt uz ekrāna otra masīva vērtības (funkcija PrintArray());
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("*****************************************************1UZD*****************************************************");
+        // Izveidojam pirmo StudijuKurss masīvu un aizpildām to ar vērtībām
+        StudijuKurss[] kurss1 = new StudijuKurss[3];
+        FillArray(kurss1);
+
+        // Izvadam pirmo masīvu uz ekrāna un saglabājam to failā
+        PrintArray(kurss1);
+        PrintArrayToFile(kurss1);
+
+        // Izveidojam otro StudijuKurss masīvu un nolasa vērtības no faila
+        StudijuKurss[] kurss2 = ReadArrayFromFile();
+
+        // Izvadam otro masīvu uz ekrāna
+        PrintArray(kurss2);
+    }
+    static void FillArray(StudijuKurss[] kurss)
+    {
+        for (int i = 0; i < kurss.Length; i++)
+        {
+            Console.WriteLine($"Ievadiet {i + 1}. kursa datus:");
+            kurss[i] = new StudijuKurss();
+            kurss[i].ReadData();
+        }
+    }
+
+    static void PrintArray(StudijuKurss[] kurss)
+    {
+        for (int i = 0; i < kurss.Length; i++)
+        {
+            kurss[i].PrintData();
+        }
+    }
+
+    static void PrintArrayToFile(StudijuKurss[] kurss)
+    {
+        using (StreamWriter writer = new StreamWriter("kursi.txt"))
+        {
+            for (int i = 0; i < kurss.Length; i++)
+            {
+                writer.WriteLine($"Nosaukums: {kurss[i].Nosaukums}\nKredīts: {kurss[i].Kreditpunkti}\nEkreditpunkti: {kurss[i].EKreditpunkti}\nObligāts?: {(kurss[i].IrObligats ? "jā" : "nē")}\n");
+            }
+        }
+    }
+
+    //static StudijuKurss[] ReadArrayFromFile()
+    //{
+    //    StreamReader sr = new StreamReader("kursi.txt");
+    //    int count = 0;
+    //    while (sr.ReadLine() != null)
+    //    {
+    //        count++;
+    //    }
+    //    sr.Close();
+
+    //    StudijuKurss[] kurss = new StudijuKurss[count];
+    //    sr = new StreamReader("kursi.txt");
+    //    for (int i = 0; i < count; i++)
+    //    {
+    //        string[] data = sr.ReadLine().Split(';');
+    //        kurss[i] = new StudijuKurss();
+    //        kurss[i].Nosaukums = data[0];
+    //        kurss[i].Kreditpunkti = int.Parse(data[1]);
+    //        kurss[i].EKreditpunkti = double.Parse(data[2]);
+    //        kurss[i].IrObligats = bool.Parse(data[3]);
+
+    //    }
+    //    sr.Close();
+
+    //    return kurss;
+    //}
+
+}
+class StudijuKurss
+{
+    public string Nosaukums;
+    public int Kreditpunkti;
+    public double EKreditpunkti { get { return Kreditpunkti * 1.5; } set { } }
+    public bool IrObligats;
+    public void ReadData()
+    {
+        Console.Write("Ievadiet kursa nosaukumu: ");
+        Nosaukums = Console.ReadLine();
+
+        Console.Write("Ievadiet kredītpunktu skaitu: ");
+        Kreditpunkti = int.Parse(Console.ReadLine());
+
+        Console.Write("Vai kursa kredītpunkti ir obligāti? (y/n): ");
+        string input = Console.ReadLine();
+        if (input == "y" || input == "yes" )
+        {
+            IrObligats = true;
+        }else
+        {
+            IrObligats = false;
+        }
+    }
+    public void PrintData()
+    {
+        Console.WriteLine($"\nKurss: {Nosaukums}\nKredītpunkti: {Kreditpunkti}\n E-Kredītpunkti: {EKreditpunkti}\nObligāts: {(IrObligats ? "Ja" : "Ne")}");
+    }
+}
+
