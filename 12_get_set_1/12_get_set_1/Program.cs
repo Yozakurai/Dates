3ad011332b09c00214823e514a785b8042e56f28
Yozakurai <deivids.saulitis@gmail.com>
2023-05-16 10:19:00 +0300

    12_get_set1

---
+++ 12_get_set_1/12_get_set_1/Program.cs
@@ -0,0 +1,67 @@
+﻿Darbinieks darbinieks = new Darbinieks();
+Console.Write("Ievadiet darbinieka vārdu: ");
+darbinieks.vards = Console.ReadLine();
+Console.Write("Ievadiet darbinieka uzvārdu: ");
+darbinieks.uzvards = Console.ReadLine();
+Console.Write("Ievadiet darbinieka dzimsanas gadu: ");
+darbinieks.dzimsanasGads = int.Parse(Console.ReadLine());
+Console.WriteLine($"Vards: {darbinieks.vards}");
+Console.WriteLine($"Uzvards: {darbinieks.uzvards}");
+Console.WriteLine($"Vecums: {darbinieks.vecums}");
+class Darbinieks
+{
+    private string Vards;
+    public string vards
+    {
+        get
+        {
+            return Vards;
+        }
+        set
+        {
+            if (value.Length > 20)
+            {
+                Vards = "Nedefinets";
+            }
+            else
+            {
+                Vards = value;
+            }
+        }
+    }
+    private string Uzvards;
+    public string uzvards
+    {
+        get
+        {
+            if (Vards == "Nedefinets")
+            {
+                return "Vards nav definēts";
+            }
+            else
+            {
+                return Uzvards;
+            }
+        }
+        set
+        {
+            Uzvards = value;
+        }
+    }
+    private int DzimsanasGads;
+    public int dzimsanasGads
+    {
+        set
+        {
+            DzimsanasGads = value;
+        }
+    }
+    public int Vecums;
+    public int vecums
+    {
+        get
+        {
+            return Vecums = 2023 - DzimsanasGads;
+        }
+    }
+}
